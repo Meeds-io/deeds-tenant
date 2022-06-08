c136fecf7764ca2b93e7b4ee23ba377bac289803
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-06-08 15:16:26 +0100

    TASK-150: create wallet after sign in with metamask (#5)
    Prior to this change when signing in with metaMask no wallet is created
    This change allows the creation of a wallet with the selected wallet from signIn

---
+++ deeds-tenant-service/src/main/java/io/meeds/tenant/metamask/listener/NewMetamaskCreatedUserListener.java
@@ -0,0 +1,72 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2022 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.tenant.metamask.listener;
+
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.User;
+import org.exoplatform.services.organization.UserEventListener;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.wallet.model.Wallet;
+import org.exoplatform.wallet.model.WalletProvider;
+import org.exoplatform.wallet.service.WalletAccountService;
+import org.web3j.crypto.WalletUtils;
+
+public class NewMetamaskCreatedUserListener extends UserEventListener {
+
+  private static final Log     LOG = ExoLogger.getLogger(NewMetamaskCreatedUserListener.class);
+
+  private IdentityManager      identityManager;
+
+  private WalletAccountService walletAccountService;
+
+  public NewMetamaskCreatedUserListener(IdentityManager identityManager, WalletAccountService walletAccountService) {
+    this.walletAccountService = walletAccountService;
+    this.identityManager = identityManager;
+  }
+
+  @Override
+  public void postSave(User user, boolean isNew) {
+    String address = user.getUserName();
+    if (!isNew || !user.isEnabled() || !WalletUtils.isValidAddress(address)) {
+      return;
+    }
+    Wallet wallet = walletAccountService.getWalletByAddress(address);
+    if (wallet != null) {
+      LOG.info("Wallet with address {} is already associated to identity id {}."
+          + "The used Metamask address will not be associated to current user account.",
+               address,
+               wallet.getTechnicalId());
+      return;
+    }
+    createUserWalletByAddress(address);
+  }
+
+  private void createUserWalletByAddress(String address) {
+    try {
+      Identity identity = identityManager.getOrCreateUserIdentity(address);
+      Wallet userWallet = walletAccountService.createWalletInstance(WalletProvider.METAMASK,
+                                                                    address,
+                                                                    Long.valueOf(identity.getId()));
+      walletAccountService.saveWallet(userWallet, true);
+    } catch (Exception e) {
+      LOG.warn("Error while associating Metamask wallet for user {}", address, e);
+    }
+  }
+
+}
