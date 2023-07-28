9127e843055c912ba49adb34c42e65cbddf55eab
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-28 17:32:09 +0100

    feat: Add WoM Connection UI - MEED-2392 - Meeds-io/MIPs#58

---
+++ deeds-tenant-webapp/src/main/webapp/vue-app/wom-integration/components/rewards/UEMRewards.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card flat>
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>{{ $t('wom.UEMRewards') }}</v-list-item-title>
+        <v-list-item-subtitle>{{ $t('wom.UEMRewardsSummary') }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-data-iterator
+      :items="rewards"
+      hide-default-footer>
+      <template #no-data>
+        <div class="align-center justify-center text-subtitle dark-grey-color">
+          <v-icon
+            size="36"
+            color="secondary"
+            class="me-4">
+            fa-hand-holding-usd
+          </v-icon>
+          {{ $t('womn.noUEMRewards') }}
+        </div>
+      </template>
+      <template #default="props">
+        <v-row>
+          <v-col
+            v-for="item in props.items"
+            :key="item.id"
+            cols="12"
+            sm="12"
+            md="6"
+            lg="4">
+            <wom-integration-uem-reward-item :reward="item" />
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    rewards: [],
+  }),
+};
+</script>
