3eb5117470f6adba35c6d1c3e1bd8bc8c4fbc610
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-01-24 18:20:50 +0100

    feat: Display Disabled Card when disconnected from WoM - MEED-3194 - Meeds-io/MIPs#106 (#55)

---
+++ deeds-tenant-webapp/src/main/webapp/vue-app/wom-setup-admin/components/connection/text/ConnectedLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p
+      class="mb-5 text-center"
+      v-sanitized-html="$t('wom.connected')"></p>
+    <p class="my-0">
+      {{ $t('wom.connected.part1') }}
+    </p>
+    <p
+      class="my-0"
+      v-sanitized-html="$t('wom.connected.part2', {
+        0: `<a href='http://www.meeds.io/hubs' target='_blank' rel='nofollow noreferrer noopener'>`,
+        1: '</a>'
+      })">
+    </p>
+    <p class="mb-5">
+      {{ $t('wom.connected.part3') }}
+    </p>
+  </div>
+</template>
