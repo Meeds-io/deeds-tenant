cdb04f12c3ff95881ac39b5c20101a26d551db67
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-23 14:17:04 +0200

    feat: Update wallet service dependency - MEED-7455 - Meeds-io/MIP#154 (#88)
    Update wallet service packaging.

--- deeds-tenant-service/src/main/java/io/meeds/tenant/metamask/listener/NewMetamaskCreatedUserListener.java
+++ deeds-tenant-service/src/main/java/io/meeds/tenant/metamask/listener/NewMetamaskCreatedUserListener.java
@@ -27,9 +27,9 @@ import org.exoplatform.services.organization.User;
 import org.exoplatform.services.organization.UserEventListener;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.manager.IdentityManager;
-import org.exoplatform.wallet.model.Wallet;
-import org.exoplatform.wallet.model.WalletProvider;
-import org.exoplatform.wallet.service.WalletAccountService;
+import io.meeds.wallet.wallet.model.Wallet;
+import io.meeds.wallet.wallet.model.WalletProvider;
+import io.meeds.wallet.wallet.service.WalletAccountService;
 
 import jakarta.annotation.PostConstruct;
 
